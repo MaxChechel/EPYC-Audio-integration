8ef128fa2f20fed9613ecf48a42da8c92ddde16f
Max Chechel <maksymchechel@yahoo.com>
2024-01-22 15:35:25 +0100

    Update main.js

--- main.js
+++ main.js
@@ -5,91 +5,62 @@ const audioModalClose = audioModal.querySelector(
 );
 const audioLinks = document.querySelectorAll('.text-rich-text a[data-audio]');
 
+let audioElement = null;
 let lastFocusedAudioElement;
-let clickAudioEventListener; // Declare a variable to store the click event listener
-let sound = null;
-let currentAudio = '';
 
 function openAudioModal(triggerElement) {
     lastFocusedAudioElement = triggerElement;
     audioModal.classList.add('is-active');
     audioPlayLink.focus();
-
-    // Close outside modal
-    clickAudioEventListener = (e) => closeModalOnClick(e, triggerElement); // Store the reference
-    document
-        .querySelector('.page-wrapper')
-        .addEventListener('click', clickAudioEventListener);
-    // Add keydown listener for Escape key when modal is open
     document.addEventListener('keydown', handleAudioModalKeydown);
 }
 
 function closeAudioModal() {
     audioModal.classList.remove('is-active');
-    if (sound) {
-        sound.stop();
+    if (audioElement) {
+        audioElement.pause();
         audioPlayLink.textContent = 'Play audio';
     }
-
     if (lastFocusedAudioElement) {
-        lastFocusedAudioElement.focus(); // Set focus back to the element that opened the modal
+        lastFocusedAudioElement.focus();
     }
-
-    // Remove keydown listener when modal is closed
     document.removeEventListener('keydown', handleAudioModalKeydown);
-
-    // Remove the click event listener using the stored reference
-    if (clickAudioEventListener) {
-        document
-            .querySelector('.page-wrapper')
-            .removeEventListener('click', clickAudioEventListener);
-    }
 }
 
 function playback() {
-    if (sound && !sound.playing()) {
-        sound.play();
-        audioPlayLink.textContent = 'Pause audio';
-    } else if (sound && sound.playing()) {
-        sound.pause();
+    if (!audioElement.paused) {
+        audioElement.pause();
         audioPlayLink.textContent = 'Play audio';
+    } else {
+        audioElement.play();
+        audioPlayLink.textContent = 'Pause audio';
     }
 }
 
-// Accessibility
-// Function to handle keydown on glossary links
-function handleAudioLinkKeydown(e) {
-    if (e.key === 'Enter') {
-        // Simulate a click event
+function handleAudioLinkKeydown(event) {
+    if (event.key === 'Enter') {
         this.click();
     }
 }
 
-// Function to handle keydown for closing modal
-function handleAudioModalKeydown(e) {
-    if (e.key === 'Escape') {
+function handleAudioModalKeydown(event) {
+    if (event.key === 'Escape') {
         closeAudioModal();
     }
 }
 
-function closeModalOnClick(e, triggerElement) {
-    if (!audioModal.contains(e.target) && e.target !== triggerElement) {
-        closeAudioModal();
-    }
-}
 document.addEventListener('DOMContentLoaded', function () {
     if (audioModal) {
         audioLinks.forEach((link) => {
             link.addEventListener('click', (e) => {
                 e.preventDefault();
-                const audio = link.getAttribute('data-audio');
+                const audioSrc = link.getAttribute('data-audio');
 
-                if (currentAudio !== audio) {
-                    if (sound) {
-                        sound.unload(); // Unload the previous sound
+                if (!audioElement || audioElement.src !== audioSrc) {
+                    if (audioElement) {
+                        audioElement.pause();
                     }
-                    sound = new Howl({ src: [audio] });
-                    currentAudio = audio;
+                    audioElement = new Audio(audioSrc);
                 }
                 closeAudioModal();
                 openAudioModal(link);
@@ -98,7 +69,6 @@ document.addEventListener('DOMContentLoaded', function () {
         });
 
         audioPlayLink.addEventListener('click', playback);
-        audioPlayLink.addEventListener('touchstart', playback);
         audioModalClose.addEventListener('click', closeAudioModal);
     }
 });
